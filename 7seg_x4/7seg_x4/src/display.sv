537b9aaeb2680fb46f9644a6c9e626078192c924
Piotr Jucha <piotrek1453@gmail.com>
2025-06-19 22:05:18 +0200

    assignment operator fix

--- 7seg_x4/7seg_x4/src/display.sv
+++ 7seg_x4/7seg_x4/src/display.sv
@@ -30,36 +30,36 @@ module display #(
   generate
     if (INVERT_SEGMENT_OUT) begin
       always_comb begin
-        segmentEnableOut[0] <= ~dotIn;
+        segmentEnableOut[0] = ~dotIn;
         case (count)
-          'd0: segmentEnableOut[7:1] <= ~7'b1111110;
-          'd1: segmentEnableOut[7:1] <= ~7'b0110000;
-          'd2: segmentEnableOut[7:1] <= ~7'b1101101;
-          'd3: segmentEnableOut[7:1] <= ~7'b1111001;
-          'd4: segmentEnableOut[7:1] <= ~7'b0110011;
-          'd5: segmentEnableOut[7:1] <= ~7'b1011011;
-          'd6: segmentEnableOut[7:1] <= ~7'b1011111;
-          'd7: segmentEnableOut[7:1] <= ~7'b1110000;
-          'd8: segmentEnableOut[7:1] <= ~7'b1111111;
-          'd9: segmentEnableOut[7:1] <= ~7'b1111011;
-          default: segmentEnableOut[7:1] <= ~7'b1111110;
+          'd0: segmentEnableOut[7:1] = ~7'b1111110;
+          'd1: segmentEnableOut[7:1] = ~7'b0110000;
+          'd2: segmentEnableOut[7:1] = ~7'b1101101;
+          'd3: segmentEnableOut[7:1] = ~7'b1111001;
+          'd4: segmentEnableOut[7:1] = ~7'b0110011;
+          'd5: segmentEnableOut[7:1] = ~7'b1011011;
+          'd6: segmentEnableOut[7:1] = ~7'b1011111;
+          'd7: segmentEnableOut[7:1] = ~7'b1110000;
+          'd8: segmentEnableOut[7:1] = ~7'b1111111;
+          'd9: segmentEnableOut[7:1] = ~7'b1111011;
+          default: segmentEnableOut[7:1] = ~7'b1111110;
         endcase
       end
     end else begin
       always_comb begin
-        segmentEnableOut[0] <= ~dotIn;
+        segmentEnableOut[0] = ~dotIn;
         case (count)
-          'd0: segmentEnableOut[7:1] <= 7'b1111110;
-          'd1: segmentEnableOut[7:1] <= 7'b0110000;
-          'd2: segmentEnableOut[7:1] <= 7'b1101101;
-          'd3: segmentEnableOut[7:1] <= 7'b1111001;
-          'd4: segmentEnableOut[7:1] <= 7'b0110011;
-          'd5: segmentEnableOut[7:1] <= 7'b1011011;
-          'd6: segmentEnableOut[7:1] <= 7'b1011111;
-          'd7: segmentEnableOut[7:1] <= 7'b1110000;
-          'd8: segmentEnableOut[7:1] <= 7'b1111111;
-          'd9: segmentEnableOut[7:1] <= 7'b1111011;
-          default: segmentEnableOut[7:1] <= 7'b1111110;
+          'd0: segmentEnableOut[7:1] = 7'b1111110;
+          'd1: segmentEnableOut[7:1] = 7'b0110000;
+          'd2: segmentEnableOut[7:1] = 7'b1101101;
+          'd3: segmentEnableOut[7:1] = 7'b1111001;
+          'd4: segmentEnableOut[7:1] = 7'b0110011;
+          'd5: segmentEnableOut[7:1] = 7'b1011011;
+          'd6: segmentEnableOut[7:1] = 7'b1011111;
+          'd7: segmentEnableOut[7:1] = 7'b1110000;
+          'd8: segmentEnableOut[7:1] = 7'b1111111;
+          'd9: segmentEnableOut[7:1] = 7'b1111011;
+          default: segmentEnableOut[7:1] = 7'b1111110;
         endcase
       end
     end
